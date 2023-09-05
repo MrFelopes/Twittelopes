f8c0391ebc79a4da13fee215baca04ed6cf9bbe3
MrFelopes <feliperfragoso@gmail.com>
2023-09-05 14:08:18 -0300

    aleluia

--- src/utils/feed.jsx
+++ src/utils/feed.jsx
@@ -1,10 +1,11 @@
 import { getDownloadURL, getStorage, list, listAll, ref } from "firebase/storage";
 import { useEffect, useState } from "react"
 import { FlatList, Image } from "react-native";
+import { View } from "react-native";
 
 
 export default function Feed(){
-    const [imagesUrl, setImagesUrl] = useState(null)
+    const [imagesUrl, setImagesUrl] = useState([])
 
     const storage = getStorage();
     useEffect(() => {
@@ -12,24 +13,23 @@ export default function Feed(){
     }, [])
 
     const setURLsToFilesInBucket = async () => {
-        const imageRefs = await listAll(ref(storage));
+        const imageRefs = await listAll(ref(storage, "images"));
         const urls = await Promise.all(imageRefs.items.map((imageRef) => getDownloadURL(imageRef)));
         setImagesUrl(urls);
     };
     
     return( 
 
-        <FlatList 
-            style={{flex: 1, flexDirection: 'row'}}
-            data = {imagesUrl}
-            keyExtractor={(item) => item}
-            renderItem={({item}) => {
-                return( 
-                    <Image
-                        style={{width: 200, height: 200}}
-                        source={{uri: imagesUrl}}
-                    />
-                )}}
-        />
+        <View>
+            {imagesUrl.map((url) => (
+                <Image
+                    style={{ width: 200, height: 200 }}
+                    source={{
+                    uri: url,
+                    }}
+                />
+            ))}
+        </View>
+        
 
     )}
